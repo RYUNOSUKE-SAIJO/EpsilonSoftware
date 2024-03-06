633ff141dc5233376cd7ee4dff672760d5530038
saijo ryunosuke <mamachari0310@gmail.com>
2024-03-06 14:24:48 +0900

    test

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+
+jQuery(function ($) {
+  var service_swiper = new Swiper(".js-mv__swiper", {
+    loop: true,
+    speed: 6000,
+    spaceBetween: 12,
+    slidesPerView: 1.7,
+    centeredSlides: true, // アクティブなスライドを中央にする
+    allowTouchMove: false, // スワイプ無効
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 20,
+        slidesPerView: 3.5,
+      },
+      500: {
+        spaceBetween: 20,
+        slidesPerView: 2,
+      },
+    },
+  });
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
